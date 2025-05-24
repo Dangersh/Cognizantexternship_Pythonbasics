f1acb66b0b7315d0c43ec6054bebd00c60a0b14d
Dangershman <dan28g@my.yorku.ca>
2025-05-24 18:24:35 -0400

    123

--- assignment 3.py
+++ assignment 3.py
@@ -1,9 +1,11 @@
+#1 Q1 - String Slicing
 python = "python is amazing"
 print(python[0:6])
 print(python[10:18])
 
 print(python[-1:-18:-1])
 
+#2 Q2 - String Methods
 python_2 = " hello, python world!."
 python_2.strip(" ")
 python_2 = python_2.capitalize()
@@ -11,8 +13,9 @@ python_2 = python_2.replace("world", "universe")
 python_2 = python_2.upper()
 print(python_2)
 
+#Q3 - Check for Palindromes
 palindrome = str(input("Enter a word: "))
 if palindrome == palindrome[::-1]:
-    print("The number is a palindrome.")
+    print("The word is a palindrome.")
 else:
-    print("The number is not a palindrome.")
+    print("The word is not a palindrome.")

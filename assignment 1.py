3847f24be9851403f12e7e801f783b117e9d015f
Dangershman <dan28g@my.yorku.ca>
2025-05-24 18:34:52 -0400

    123

--- assignment 1.py
+++ assignment 1.py
@@ -1,8 +1,14 @@
+#Q1
+print ("Q1")
 name = "dan"
 age = 20
 height = 5.10
 
 print(f"Hey there my name is {name}! I am {age} years old and {height:.2f} feet tall")
+
+#Q2
+print ("Q2")
+print()
 
 num_1 = 10
 num_2 = 20
@@ -12,7 +18,9 @@ print (f"Subtraction of {num_1} and {num_2} is {num_1 - num_2}")
 print (f"Multiplication of {num_1} and {num_2} is {num_1 * num_2}")
 print (f"Division of {num_1} and {num_2} is {num_1 / num_2}")
 
-
+#Q3
+print ("Q3")
+print()
 num = float(input("Enter a number: "))
 
 if num > 0:
